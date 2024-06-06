c6f976511485b746401124dc0221808672716df0
numbhill <123810757+numbhill@users.noreply.github.com>
2024-06-06 03:58:46 +0200

    Update welcome.blade.php
    Github Repo Button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
             <a href="{{ route('knapsack.index') }}"
                class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Try the Knapsack
                 Function</a>
+            <a href="https://github.com/numbhill/knapsack-project"
+               class="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Github Repo</a>
         </div>
     </div>
     <div class="mt-10">
